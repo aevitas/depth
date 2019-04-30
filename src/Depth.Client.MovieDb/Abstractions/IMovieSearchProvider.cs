3fd419b0e49956effad3598b76f8971c13c64137
aevitas <m4rtijn@gmail.com>
2019-04-30 11:34:54 +0200

    Add initial MovieDb Client stuff

---
+++ src/Depth.Client.MovieDb/Abstractions/IMovieSearchProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Depth.Client.MovieDb.Models;
+
+namespace Depth.Client.MovieDb.Abstractions
+{
+    public interface IMovieSearchProvider
+    {
+        Task<IEnumerable<Movie>> SearchAsync(Action<SearchOptions> options);
+    }
+}
---
+++ src/Depth.Client.MovieDb/SearchOptions.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+
+namespace Depth.Client.MovieDb
+{
+    public sealed class SearchOptions
+    {
+        public string Query { get; set; }
+
+        public string Language { get; set; }
+
+        public int? Page { get; set; }
+
+        [JsonProperty("include_adult")]
+        public bool IncludeAdult { get; set; }
+
+        public string Region { get; set; }
+
+        public int? Year { get; set; }
+
+        [JsonProperty("primary_release_year")]
+        public int? PrimaryReleaseYear { get; set; }
+    }
+}
